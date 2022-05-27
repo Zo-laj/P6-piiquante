8e54bc6aa1443587fbf73108b0738997912e9a5c
zo-laj <zoe.lajoie2@gmail.com>
2022-05-27 18:09:30 +0200

    mise en place de l'environnement, initialisation du projet node express mongodb

---
+++ back-end/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//JKwlPRuoeta0adhE
+//mongodb+srv://Zo-laj:<password>@cluster0.xiawemn.mongodb.net/?retryWrites=true&w=majority
+
+const app = express();
+
+mongoose.connect('mongodb+srv://Zo-laj:JKwlPRuoeta0adhE@cluster0.xiawemn.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
